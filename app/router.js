ec42b9f363b5190c2c027fb52dd8757fe1fa6ae0
Sabrina  Ludovic de Lys <sabrinaludovicdelys@Sabrinas-MacBook-Air.local>
2021-04-01 10:20:21 +0200

    setting up routes

---
+++ app/router.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router();
+
+// Importer les controllers
+
+const articleController = require('./controllers/articleController');
+const countryController = require('./controllers/countryController');
+const associationController = require('./controller/associationController');
+
+
+
+// Récupérer tous les articles
+router.get('/articles', articleController.findAll);
+
+// Récupérer un article 
+// router.get('/articles/:id', articleController.findOne);
+
+
+// Récupérer tout les pays 
+// router.get('/countries', countryController.findAll);
+
+// Récupérer tout les articles d'un pays 
+// router.get('/countries/:countryId/articles')
+
+
+// Ajouter un article en tant que admin 
