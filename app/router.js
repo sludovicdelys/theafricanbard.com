7b4a1c51ef10bff2d5de481e595b3acba3424db8
Sabrina  Ludovic de Lys <sabrinaludovicdelys@Sabrinas-MacBook-Air.home>
2021-04-13 18:10:56 +0200

    clean up

--- app/router.js
+++ app/router.js
@@ -14,6 +14,7 @@ router.get('/', mainController.showHomePage);
 
 // Récupérer tous les articles
 router.get('/articles', articleController.findAll);
+router.get()
 
 // Récupérer un article 
 router.get('/articles/:id', articleController.findOne);
