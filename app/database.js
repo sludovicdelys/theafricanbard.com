1da1d14ab9f510e9b7b644c54248f324765cb436
Sabrina  Ludovic de Lys <sabrinaludovicdelys@Sabrinas-MacBook-Air.home>
2021-05-04 20:54:18 +0200

    heroku database postgresql

--- app/database.js
+++ app/database.js
@@ -6,6 +6,12 @@ const sequelize = new Sequelize(process.env.PG_URL, {
     define: {
         timestamps:false,
         underscored: true
+    },
+    dialectOptions: {
+        ssl: {
+          require: true, // This will help you. But you will see nwe error
+        //   rejectUnauthorized: false // This line will fix new error
+        }
     }
 });
 
